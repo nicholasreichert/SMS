ff7b59a91bd56597eed851ec5b5fa90b997a4015
Nicholas Reichert <nicholasreichert@Nicholass-Air.fios-router.home>
2023-03-02 21:30:43 -0500

    Initial commit

---
+++ Student_Management_System/General_Information/basicKnowledge.java
@@ -0,0 +1,24 @@
+
+
+
+public class basicKnowledge
+{
+    // instance fields
+    private String name;
+    private int age;
+    private int grade;
+    private int student_id_number;
+
+    // constructors
+
+    public basicKnowledge(String full_name, int age, int grade)
+    {
+        name = full_name;
+        age = this.age;
+        grade = this.grade;
+        for(int i = 0; i <=10; i++)
+        {
+
+        }
+    }
+}
